278a5230c11e6adbe02eb0870c6dfb70ae601fb1
MarcusStill <belg.marcus@gmail.com>
2025-02-04 18:22:03 +0300

    Update snapshot city_region.

--- snapshots/dictionaries/snap_city_region.sql
+++ snapshots/dictionaries/snap_city_region.sql
@@ -2,12 +2,18 @@
 
 {{
    config(
-       target_schema='snapshot',
-       unique_key='city',
+        target_schema='snapshot',
+        unique_key='city',
 
-       strategy='timestamp',
-       updated_at='updated_at',
-       dbt_valid_to_current ="'9999-01-01'::date"
+        strategy='timestamp',
+        updated_at='updated_at',
+        dbt_valid_to_current = "'9999-01-01'::date",
+
+        hard_deletes='new_record',
+
+        snapshot_meta_column_names={
+            "dbt_is_deleted": "dbt_is_deleted"
+        }
    )
 }}
 
