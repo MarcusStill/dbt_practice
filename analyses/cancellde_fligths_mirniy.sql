420b63bc58137748b76b56c6a325b16e0dd133ba
MarcusStill <belg.marcus@gmail.com>
2025-02-12 11:19:24 +0300

    Added analyses

---
+++ analyses/cancellde_fligths_mirniy.sql
@@ -0,0 +1,10 @@
+select
+    scheduled_departure::date as scheduled_departure,
+    count(*) as cancelled_cnt
+from
+    {{ ref('fct_flights') }}
+where 
+    departure_airport = 'MJZ'
+    and status = 'Cancelled'
+group by
+    scheduled_departure::date
