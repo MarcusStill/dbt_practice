36ae2eefa37d71e283bc6876837959d275e63a03
MarcusStill <belg.marcus@gmail.com>
2025-04-15 22:00:40 +0300

    Added a check for the days parameter and error generation if it is negative.

--- macros/limit_data_dev.sql
+++ macros/limit_data_dev.sql
@@ -1,4 +1,7 @@
 {% macro limit_data_dev(column_name, days=5000) %}
+{% if days < 0 %}
+  {{ exceptions.raise_compiler_error("Error in the value passed in the 'days' parameter. Got: " ~ days) }}
+{% endif %}
 {% if target.name == 'dev' %}
 WHERE
     {{ column_name }} >= current_date - interval '{{ days }} days'
