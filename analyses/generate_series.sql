f6880e72e38c4f2b87e8c381d9e5cb695c6b1b27
MarcusStill <belg.marcus@gmail.com>
2025-04-20 22:33:09 +0300

    Подключил dbt_utils и обавил генератор дат

---
+++ analyses/generate_series.sql
@@ -0,0 +1,7 @@
+{# Генератор sql-кода для получения списка дат #}
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2024-01-01' as date)",
+    end_date="cast('2025-01-01' as date)"
+   )
+}}
