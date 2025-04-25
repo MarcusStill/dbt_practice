b8ca701db8068a8830bfb6d9975ebe6aaf2a4f32
MarcusStill <belg.marcus@gmail.com>
2025-04-25 20:38:39 +0300

    Эксперимент с dbt ls

--- models/staging/flights/stg_flights__seats.sql
+++ models/staging/flights/stg_flights__seats.sql
@@ -8,5 +8,12 @@ select
     aircraft_code,
     seat_no,
     fare_conditions
+    --, 'static_value' as seat_group  -- добавляем новое поле
 from {{ source('demo_src', 'seats') }}
-  
+
+{# Сохраняем текущий manifest.json:
+mkdir state_snapshot
+copy target\manifest.json state_snapshot\manifest.json
+
+Запускаем dbt ls с указанием изменений:
+dbt ls --select state:modified --state state_snapshot/ #}
